021ac595052d6d438aaf628fe71317d920741602
rohith7577 <67580125+rohith7577@users.noreply.github.com>
2020-08-02 16:54:18 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,13 +19,13 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
 
-hai {}, my name is {}! if you have any questions about how to use me please give me /help... 
+hello {}, Iam a bot and my name is {}😎.
 
 im a group manager bot maintained by  [this person](tg://user?id={}).
 
-For more commands click /help...
+No use in messaging me here am available on @BTECHTHOPULU
 
-**U r not able to any thing in this so be silent.**
+**U r not able to do any thing in this so be silent.**
 
 """
 
@@ -50,10 +50,10 @@ And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll of the following commands  / or ! can  be used...\n")
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
-It took lots of work for [my creator](t.me/SonOfLars) to get me to where I am now, and every donation helps \
+It took lots of work for [my creator](t.me/roHiTh_rio) to get me to where I am now, and every donation helps \
 motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
 (see his bio!). He's just a poor student, so every little helps!
-There are two ways of paying him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
+There are two ways of paying him; [PayPal](paypal.me/rohith75779652), ."""
 
 IMPORTED = {}
 MIGRATEABLE = []
